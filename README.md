d5dc7122325bb86a39224bd72900c6b6e7ef1a92
alex - windows <alicelukas@naver.com>
2020-12-15 03:50:12 +0900

    initial commit
    - nodejs-mysql-docker 그대로 복붙

---
+++ README.md
@@ -0,0 +1,23 @@
+# nodejs-mysql-docker
+nodejs mysql docker simple example
+
+이 github의 베이스는  
+https://github.com/amalendukundu/myonlineedu-nodejs-mysql-docker 이 곳입니다.
+
+## Resources
+
+* [Read MyOnlineEdu Blog for details](https://www.myonlineedu.com/blog/view/14/build-docker-container-for-nodejs-and-mysql-based-application)
+* [유튜브](https://youtu.be/RR_oMDp4kEc)
+
+--------------
+## 설치 및 실행(docker build)
+install.bat
+
+## 실행 or 새로고침(docker up)
+run.bat
+
+## 개발
+npm run r
+
+## 컨테이너 및 이미지 삭제
+uninstall.bat
---
+++ init/init.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: moe-mysql-app:3306
+-- 생성 시간: 20-12-09 07:25
+-- 서버 버전: 5.7.32
+-- PHP 버전: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `moe_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `MOE_ITEM_T`
+--
+
+CREATE TABLE `MOE_ITEM_T` (
+  `ITEM_NAME` varchar(50) NOT NULL,
+  `ITEM_DESC` varchar(100) NOT NULL,
+  `ITEM_ONHAND` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 테이블의 덤프 데이터 `MOE_ITEM_T`
+--
+
+INSERT INTO `MOE_ITEM_T` (`ITEM_NAME`, `ITEM_DESC`, `ITEM_ONHAND`) VALUES
+('TEST-ITEM-1', 'TEST-ITEM-DESC-1', 10),
+('TEST-ITEM-2', 'TEST-ITEM-DESC-2', 20);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
